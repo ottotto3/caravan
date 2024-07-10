79bc14a60a2b5e2bf4e92e8d0409765113687484
ottotto3 <xufangyi8@gmail.com>
2024-07-10 17:39:32 +0900

    240710

---
+++ progeta/3-0710.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    hello();
+  }
+
+  public static void hello() {
+    // "Hello World"を、"Hello Java"に書き換えてください
+    System.out.println("Hello Java");
+  }
+
+  public static void main(String[] args) {
+    // printDataメソッドを呼び出してください
+    printData();
+
+  }
+
+  // printDataメソッドを定義してください
+  public static void printData() {
+    System.out.println("私の名前はKate Jonesです");
+  }
+}
